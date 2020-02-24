80265cdb641c464ac64115c961c333f97d1367bb
Hoon Yang <ssang276@hanmail.net>
2020-02-24 13:36:19 +0900

    Vuetify Menu Layout

--- rbac/src/main/resources/static/index.js
+++ rbac/src/main/resources/static/index.js
@@ -1,5 +1,29 @@
+// https://vuetifyjs.com/ko/customization/theme#%EB%B0%9D%EA%B2%8C-%EC%96%B4%EB%91%A1%EA%B2%8C-light-and-dark
+var vuetify = new Vuetify({
+    theme: { dark: true }
+})
+
+// https://www.npmjs.com/package/http-vue-loader
+// https://medium.com/@jamesweee/using-vue-js-single-file-component-without-module-bundlers-aea58d892ad9
+// - Custom Component
+
 var app = new Vue({
     el: '#app',
-    vuetify: new Vuetify(),
-    data: {}
+    // vuetify: new Vuetify(),
+    vuetify: vuetify,
+    data: {
+        drawer: true,
+        items: {
+            'Applications': [
+                { title: 'CI/CD', icon: 'mdi-image' },
+                { title: 'xxx', icon: 'mdi-help-box' }
+            ],
+            'Administrator': [
+                { title: 'Realms', icon: 'mdi-image' },
+                { title: 'Projects', icon: 'mdi-help-box' }
+            ]
+        },
+        selected: 'Realms',
+        miniVariant: false
+    }
 })
